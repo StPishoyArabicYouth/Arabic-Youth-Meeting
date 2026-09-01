f2fbdd50cbb71f7831e1b15588ae58f6bb215e0a
StPishoyArabicYouth <shre300@yahoo.com>
2026-09-01 09:56:30 -0500

    Upgrade service worker to version 3 with enhanced caching
    Updated service worker to version 3 with improved caching strategy and added support for handling fetch requests more effectively.

--- sw.js
+++ sw.js
@@ -1,5 +1,32 @@
-const CACHE='arabic-youth-final-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./conference-2026.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'arabic-youth-final-v3';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./conference-2026.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || e.request.headers.has('range')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
